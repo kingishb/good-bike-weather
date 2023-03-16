12718d1e3acbfd2ccd8c9abc25ff6aec8164ffb7
Brian King <brian.king@adhocteam.us>
2023-03-15 22:39:30 -0400

    update docstring

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 """
-Script to send a daily digest for weather forecasts in the next week that are 
+Script to send a daily digest of weather forecasts in the next week that are 
 temperate, clear, and low-ish wind so  I can plan a long bike ride.
 
 Requires PUSHOVER_USER and PUSHOVER_TOKEN env vars to send push notifications.
